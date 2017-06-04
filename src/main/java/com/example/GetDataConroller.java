c0459655fa810d0eb92a8f9f2e2bc56d89161405
Nogok <slavanazarenko0@gmail.com>
2017-06-04 20:15:25 +0300

    fixing

--- src/main/java/com/example/GetDataConroller.java
+++ src/main/java/com/example/GetDataConroller.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.tokens.BlockSequenceStartToken;
 
 import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
