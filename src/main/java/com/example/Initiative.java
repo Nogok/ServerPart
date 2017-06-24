2cdade3ebb13e24896d0023f1a89930edf96262d
Nogok <slavanazarenko0@gmail.com>
2017-06-24 09:19:43 +0300

    Initiative

--- src/main/java/com/example/Initiative.java
+++ src/main/java/com/example/Initiative.java
@@ -3,16 +3,20 @@ package com.example;
 import java.util.Arrays;
 
 public class Initiative {
-   public String name;
+  
    public String description;
+   public String name;
    public String[] variants;
+   
    public Initiative() {
    }
-   public Initiative(String name,String description,String[] variants){
-	   this.name = name;
+   
+   public Initiative(String description,String name,String[] variants){
 	   this.description=description;
+	   this.name = name;
 	   this.variants=variants;
    }
+   
    @Override
 	public boolean equals(Object obj) {
 	   if (this == obj)
